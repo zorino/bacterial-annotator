f563d3b6232d26b070f7f815a40ac027f75df42c
zorino <maximilien1er@gmail.com>
2016-09-23 12:00:02 +0000

    add option to compare several annotation - no implementation yet
    Signed-off-by: zorino <maximilien1er@gmail.com>

--- test/launch-ecoli-annotation.sh
+++ test/launch-ecoli-annotation.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-bacterial-annotator -i data/Illumina_2x300bp_Ecoli_RayAssembly.fasta -g  U00096.3 -o Ecoli-RayAssembly-Annotation -f "$@"
+bacterial-annotator annotate -i data/Illumina_2x300bp_Ecoli_RayAssembly.fasta -g  U00096.3 -o Ecoli-RayAssembly-Annotation -f "$@"
 
--- test/launch-ecoli-annotation_REF+NRremote.sh
+++ test/launch-ecoli-annotation_REF+NRremote.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-bacterial-annotator -i data/Illumina_2x300bp_Ecoli_RayAssembly.fasta\
+bacterial-annotator annotate -i data/Illumina_2x300bp_Ecoli_RayAssembly.fasta\
 		    -g U00096.3\
 		    --remotedb nr\
 		    -o Ecoli-RayAssembly-Annotation-REF+NRremote\
